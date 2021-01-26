673bfddc61a4ffd57cdb70afbb889fd1c3394046
Xkein <739502808@qq.com>
2021-01-26 20:36:31 +0800

    update yrpp.

---
+++ PatcherYRpp/CCINIClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatcherYRpp
+{
+    [StructLayout(LayoutKind.Explicit, Size = 88)]
+    public struct INIClass
+    {
+
+    }
+
+    [StructLayout(LayoutKind.Explicit, Size = 88)]
+    public struct CCINIClass
+    {
+        private static IntPtr pINI_Rules = new IntPtr(0x887048);
+        public static Pointer<CCINIClass> INI_Rules { get => ((Pointer<Pointer<CCINIClass>>)pINI_Rules).Data; set => ((Pointer<Pointer<CCINIClass>>)pINI_Rules).Ref = value; }
+
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        delegate int ReadStringFunction(ref CCINIClass pThis, [MarshalAs(UnmanagedType.LPStr)]string pSection,
+            [MarshalAs(UnmanagedType.LPStr)] string pKey, [MarshalAs(UnmanagedType.LPStr)] string pDefault, byte[] buffer, int bufferSize);
+        static ReadStringFunction ReadStringDlg = Marshal.GetDelegateForFunctionPointer<ReadStringFunction>(new IntPtr(0x528A10));
+
+        public int ReadString(string section, string key, string def, byte[] buffer, int bufferSize)
+        {
+            return ReadStringDlg(ref this, section, key, def, buffer, bufferSize);
+        }
+
+        [FieldOffset(0)]
+        public INIClass Base;
+    }
+}
--- PatcherYRpp/LaserDrawClass.cs
+++ PatcherYRpp/LaserDrawClass.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using static PatcherYRpp.YRPP;
 
 namespace PatcherYRpp
 {
     [StructLayout(LayoutKind.Explicit, Size = 92)]
     public struct LaserDrawClass
     {
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        delegate IntPtr ConstructorFunction(ref LaserDrawClass pThis, CoordStruct source, CoordStruct target, int zAdjust, byte unknown,
+            ColorStruct innerColor, ColorStruct outerColor, ColorStruct outerSpread,
+            int duration, bool blinks, bool fades, float startIntensity, float endIntensity);
+
+        static ConstructorFunction ConstructorDlg = Marshal.GetDelegateForFunctionPointer<ConstructorFunction>(new IntPtr(0x54FE60));
+        static DestructorFunction DestructorDlg = Marshal.GetDelegateForFunctionPointer<DestructorFunction>(new IntPtr(0x54FFB0));
+
+        public static void Constructor(Pointer<LaserDrawClass> pThis, CoordStruct source, CoordStruct target, int zAdjust, byte unknown,
+            ColorStruct innerColor, ColorStruct outerColor, ColorStruct outerSpread,
+            int duration, bool blinks = false, bool fades = true,
+            float startIntensity = 1.0f, float endIntensity = 0.0f)
+        {
+            ConstructorDlg(ref pThis.Ref, source, target, zAdjust, unknown, innerColor, outerColor, outerSpread, duration, blinks, fades, startIntensity, endIntensity);
+        }
+
+        public static void Constructor(Pointer<LaserDrawClass> pThis, CoordStruct source, CoordStruct target, ColorStruct innerColor,
+            ColorStruct outerColor, ColorStruct outerSpread, int duration)
+        {
+            Constructor(pThis, source, target, 0, 1, innerColor, outerColor, outerSpread, duration);
+        }
+
+        public void Destructor()
+        {
+            DestructorDlg(Pointer<LaserDrawClass>.AsPointer(ref this));
+        }
+
         [FieldOffset(28)] public int Thickness; // only respected if IsHouseColor
         [FieldOffset(32)] public bool IsHouseColor;
         [FieldOffset(33)] public bool IsSupported; // this changes the values for InnerColor (false: halve, true: double), HouseColor only
