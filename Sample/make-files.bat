98017acc6a47df0057f6424b9c6b93b6e1e26e56
Xkein <739502808@qq.com>
2021-03-09 23:06:02 +0800

    complete Release build setting for user.

--- Sample/make-files.bat
+++ Sample/make-files.bat
@@ -2,11 +2,11 @@
 cd /d %~dp0
 
 del /q DynamicPatcher\Libraries
-for /r ..\bin\Debug %%i in (*.dll) do copy %%i DynamicPatcher\Libraries
+for /r ..\bin\Release %%i in (*.dll) do copy %%i DynamicPatcher\Libraries
 del .\DynamicPatcher\Libraries\DynamicPatcher.dll
 
-copy ..\PatcherLoader\Debug\PatcherLoader.dll
-copy ..\PatcherLoader\Debug\DynamicPatcher.dll
+copy ..\PatcherLoader\Release\PatcherLoader.dll
+copy ..\PatcherLoader\Release\DynamicPatcher.dll
 
 copy ..\LICENSE.MD DynamicPatcherLICENSE.MD
 
