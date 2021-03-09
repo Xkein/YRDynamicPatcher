4da43a95330ece74f2391173c5c0f6df7857e3e1
Xkein <739502808@qq.com>
2021-03-09 17:16:26 +0800

    update sample for preview6.

--- Sample/DynamicPatcher/ExtensionHooks/TechnoExt.cs
+++ Sample/DynamicPatcher/ExtensionHooks/TechnoExt.cs
@@ -52,11 +52,49 @@ namespace ExtensionHooks
             }
 			catch (Exception e)
 			{
-                Helpers.PrintException(e);
+                Logger.PrintException(e);
 				return (uint)0;
 			}
         }
-        
+        [Hook(HookType.AresHook, Address = 0x6F6CA0, Size = 7)]
+        static public unsafe UInt32 TechnoClass_Put_Script(REGISTERS* R)
+        {
+            Pointer<TechnoClass> pTechno = (IntPtr)R->ECX;
+            var pCoord = R->Stack<Pointer<CoordStruct>>(0x4);
+            var faceDir = R->Stack<int>(0x8);
+
+            TechnoExt ext = TechnoExt.ExtMap.Find(pTechno);
+            ext.Scriptable?.OnPut(pCoord.Data, faceDir);
+
+            return 0;
+        }
+        [Hook(HookType.AresHook, Address = 0x6F6AC0, Size = 5)]
+        static public unsafe UInt32 TechnoClass_Remove_Script(REGISTERS* R)
+        {
+            Pointer<TechnoClass> pTechno = (IntPtr)R->ECX;
+
+            TechnoExt ext = TechnoExt.ExtMap.Find(pTechno);
+            ext.Scriptable?.OnRemove();
+
+            return 0;
+        }
+        [Hook(HookType.AresHook, Address = 0x701900, Size = 6)]
+        static public unsafe UInt32 TechnoClass_ReceiveDamage_Script(REGISTERS* R)
+        {
+            Pointer<TechnoClass> pTechno = (IntPtr)R->ECX;
+            var pDamage = R->Stack<Pointer<int>>(0x4);
+            var distanceFromEpicenter = R->Stack<int>(0x8);
+            var pWH = R->Stack<Pointer<WarheadTypeClass>>(0xC);
+            var pAttacker = R->Stack<Pointer<ObjectClass>>(0x10);
+            var ignoreDefenses = R->Stack<bool>(0x14);
+            var preventPassengerEscape = R->Stack<bool>(0x18);
+            var pAttackingHouse = R->Stack<Pointer<HouseClass>>(0x1C);
+
+            TechnoExt ext = TechnoExt.ExtMap.Find(pTechno);
+            ext.Scriptable?.OnReceiveDamage(pDamage.Data, distanceFromEpicenter, pWH, pAttacker, ignoreDefenses, preventPassengerEscape, pAttackingHouse);
+
+            return 0;
+        }
         [Hook(HookType.AresHook, Address = 0x6FDD50, Size = 6)]
         static public unsafe UInt32 TechnoClass_Fire(REGISTERS* R)
         {
@@ -65,7 +103,7 @@ namespace ExtensionHooks
             }
 			catch (Exception e)
 			{
-                Helpers.PrintException(e);
+                Logger.PrintException(e);
 				return (uint)0;
 			}
         }
