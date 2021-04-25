5abb9f0ab531af3611737ad0d13315c4bd1be865
Xkein <739502808@qq.com>
2021-04-25 23:49:42 +0800

    update release.yml and make-files.bat.

--- Sample/make-files.bat
+++ Sample/make-files.bat
@@ -1,13 +1,19 @@
-
+ 
 cd /d %~dp0
 
-del /q DynamicPatcher\Libraries
-for /r ..\bin\Release %%i in (*.dll) do copy %%i DynamicPatcher\Libraries
-del .\DynamicPatcher\Libraries\DynamicPatcher.dll
+set Libraries=DynamicPatcher\Libraries
+
+if exist %Libraries% (
+	del /q %Libraries%
+) else (
+	mkdir %Libraries%
+)
+for /r ..\bin\Release %%i in (*.dll) do copy %%i %Libraries%
+del .\%Libraries%\DynamicPatcher.dll
 
 copy ..\PatcherLoader\Release\PatcherLoader.dll
-copy ..\PatcherLoader\Release\DynamicPatcher.dll
+copy ..\PatcherLoader\Release\DynamicPatcher.dll DynamicPatcher_RELEASE.dll
+copy ..\PatcherLoader\Debug\DynamicPatcher.dll
 
 copy ..\LICENSE.MD DynamicPatcherLICENSE.MD
 
-pause
