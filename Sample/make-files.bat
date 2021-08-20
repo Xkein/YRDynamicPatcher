84ded79f24c1f6dc4d3df8b3e006c86f509df84e
Xkein <739502808@qq.com>
2021-08-21 03:40:47 +0800

    add Sample submodule.

--- Sample/make-files.bat
+++ Sample/make-files.bat
@@ -1,16 +1,6 @@
  
 cd /d %~dp0
 
-set Libraries=DynamicPatcher\Libraries
-
-if exist %Libraries% (
-	del /q %Libraries%
-) else (
-	mkdir %Libraries%
-)
-for /r ..\bin\Release %%i in (*.dll) do copy %%i %Libraries%
-del .\%Libraries%\DynamicPatcher.dll
-
 copy ..\PatcherLoader\Release\PatcherLoader.dll
 copy ..\PatcherLoader\Release\DynamicPatcher.dll DynamicPatcher_RELEASE.dll
 copy ..\PatcherLoader\Debug\DynamicPatcher.dll
